421a5e2d03ea59ec92a751b077e8d3fd6282ae98
Tejovanth N <tejovanth.n@gmail.com>
2014-03-04 23:35:36 +0530

    added cwd in routes

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import urllib2
 from threading import Timer
+import os
 
 app = Flask(__name__)
 
@@ -14,7 +15,7 @@ def route_sheet():
     url = "https://spreadsheets.google.com/feeds/list/0Aj5Gl_JbMYf5dDRDMzMtMlVzdTc0dTlpVngzMnYyTkE/od6/public/values?alt=json-in-script"
     result = urllib2.urlopen(url).read()
     apijson = result[result.index("(") + 1: result.rindex(")")]
-    f = open("static/json/full_route.json", "w+")
+    f = open(os.getcwd()+"/static/json/full_route.json", "w+")
     f.write(apijson)
     f.close()
     t = Timer(60*60*24, route_sheet)
@@ -25,7 +26,7 @@ def tweet_sheet():
     url = "https://spreadsheets.google.com/feeds/list/0Aj5Gl_JbMYf5dEZXQ0k2eVhMUG4teWgxbFlRQUM4a3c/oaw/public/values?alt=json-in-script"
     result = urllib2.urlopen(url).read()
     apijson = result[result.index("(") + 1: result.rindex(")")]
-    f = open("static/json/tweets.json", "w+")
+    f = open(os.getcwd+"/static/json/tweets.json", "w+")
     f.write(apijson)
     f.close()
     t = Timer(60*60*1, tweet_sheet)
