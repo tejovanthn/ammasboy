0fd5d70623d41a11da58f75f0d90fcf0033c7f1b
Tejovanth N <tejovanth.n@gmail.com>
2014-03-12 13:31:31 +0530

    Removes APScheduler, using Heroku Scheduler

--- update.py
+++ update.py
@@ -1,11 +1,7 @@
 import urllib2
 import os
-from apscheduler.scheduler import Scheduler
-
-sched = Scheduler()
 
 
-@sched.interval_schedule(minutes=1)
 def route_sheet():
     print "Downloading route sheet"
     url = "https://spreadsheets.google.com/feeds/list/0Aj5Gl_JbMYf5dDRDMzMtMlVzdTc0dTlpVngzMnYyTkE/od6/public/values?alt=json-in-script"
@@ -16,7 +12,6 @@ def route_sheet():
     f.close()
 
 
-@sched.interval_schedule(minutes=1)
 def tweet_sheet():
     print "Downloading tweet sheet"
     url = "https://spreadsheets.google.com/feeds/list/0Aj5Gl_JbMYf5dEZXQ0k2eVhMUG4teWgxbFlRQUM4a3c/oaw/public/values?alt=json-in-script"
@@ -26,9 +21,3 @@ def tweet_sheet():
     f.write(apijson)
     f.close()
 
-
-if __name__ == '__main__':
-    sched.start()
-
-while True :
-    pass
