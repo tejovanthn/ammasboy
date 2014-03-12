8157dc445c14f563b5a05de18d5c7b55a1b8185f
Tejovanth N <tejovanth.n@gmail.com>
2014-03-12 15:39:39 +0530

    Please enter the commit message for your changes. Lines starting

--- routes.py
+++ routes.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 import os
 import time
+import update
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    update.route_sheet()
+    update.tweet_sheet()
     return render_template('home.html', update=time.ctime(os.path.getmtime(os.getcwd()+"/static/json/tweets.json")))
 
 
