bd56bfec830349c1ed8d04eacae038aa2bc2faa6
Tejovanth N <tejovanth.n@gmail.com>
2014-11-06 01:37:57 +0530

    test

--- routes.py
+++ routes.py
@@ -18,21 +18,21 @@ def map():
 def cal():
     return render_template('calendar.html', update=time.ctime(os.path.getmtime(os.getcwd()+"/static/json/tweets.json")))
 
-@app.before_request
-def before_request():
-    g.version = "0.1"
-    g.request_start_time = time.time()
-    g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
-    if "city" not in session :
-        geo =  geocoder.ip(request.remote_addr)
-        if geo.ip is None:
-            session["city"]="Bangalore"
-        else:
-            session["city"] = re.match("^(.+),",geo.address).group(1)
-        g.city = session["city"]
-    else :
-        g.city = session["city"]
-
+#@app.before_request
+#def before_request():
+#    g.version = "0.1"
+#    g.request_start_time = time.time()
+#    g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
+#    if "city" not in session :
+#        geo =  geocoder.ip(request.remote_addr)
+#        if geo.ip is None:
+#            session["city"]="Bangalore"
+#        else:
+#            session["city"] = re.match("^(.+),",geo.address).group(1)
+#        g.city = session["city"]
+#    else :
+#        g.city = session["city"]
+#
 
 
 
